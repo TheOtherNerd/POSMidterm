3539e7d3ed47cc69179f1345d2b4d2528156b9be
Austin Howell <austinhowell@Austins-MacBook-Pro.local>
2022-04-28 14:19:09 -0400

    adding receipt method

--- POSMidterm/Order.cs
+++ POSMidterm/Order.cs
@@ -18,6 +18,13 @@ namespace POSMidterm
             ItemsForSale.Add(new Product("Cookie", CategoryType.Pastry, "Warm chocolate chip", .99));
             ItemsForSale.Add(new Product("Mug", CategoryType.Merch, "Cup with store logo", 9.99));
             ItemsForSale.Add(new Product("Egg Sandwich", CategoryType.Meal, "Delicious egg sandwich", 7.99));
+            ItemsForSale.Add(new Product("Croissant", CategoryType.Pastry, "Warm Cross-ant!", 1.99));
+            ItemsForSale.Add(new Product("Espresso", CategoryType.Drink, "Double Shot when you're double tired", 3.99));
+            ItemsForSale.Add(new Product("Grilled Cheese", CategoryType.Meal, "Bread, Cheese - Hot", 3.99));
+            ItemsForSale.Add(new Product("Insulated Thermos", CategoryType.Merch, "Keep it HOT! (or cold)", 29.99));
+            ItemsForSale.Add(new Product("Tea Set", CategoryType.Merch, "Make your own tea! At home or on the go!", 59.99));
+            ItemsForSale.Add(new Product("Cupcake", CategoryType.Pastry, "It's a cup, made of cake.", 2.99));
+            ItemsForSale.Add(new Product("Bag of Coffee Beans", CategoryType.Drink, "Make your own damn coffee!", 14.99));
         }
 
         public List<Product> AddToCart(int index, int quantity)
@@ -28,5 +35,26 @@ namespace POSMidterm
             }
             return ItemsPurchased;
         }
+
+        public double PrintRecipt()
+        {
+            Console.WriteLine("Receipt");
+            Console.WriteLine("=============");
+            double subtotal = 0;
+            for (int i = 0; i < ItemsPurchased.Count; i++)
+            {
+                Console.WriteLine(ItemsPurchased[i].ProductName);
+                subtotal += ItemsPurchased[i].Price;
+            }
+            Console.WriteLine($"Subtotal: ${ Math.Round(subtotal, 2)}");
+            double taxAmount = .065 * subtotal;
+            double grandTotal = subtotal + taxAmount;
+            Console.WriteLine($"Tax: {Math.Round(taxAmount, 2)}");
+            Console.WriteLine($"Total: {Math.Round(grandTotal, 2)}");
+            
+            return grandTotal;
+        }
     }
 }
+
+
--- POSMidterm/Program.cs
+++ POSMidterm/Program.cs
@@ -2,74 +2,78 @@
 {
     public class Program
     {
+        static Order o = new Order();
         public static void Main()
         {
-            Order o = new Order();
+            
             Menu m = new Menu("",CategoryType.Drink,"", 5);
+            Helper h = new Helper();
             int userInput;
             int desiredQuantity;
+            bool runAgain = true;
             
-
             Console.WriteLine("Welcome to our Coffee Shop. Please take a look at our menu. ");
-
-            m.PrintTable();
-            Console.WriteLine();
-
-            while (true)
+            while (runAgain)
             {
-                Console.WriteLine("To order an item, please enter the number that corresponds with the desired item.");
-                try
+                m.PrintTable();
+                Console.WriteLine();
+
+                while (true)
                 {
-                    userInput = int.Parse(Console.ReadLine());
-                    if (userInput < 0 || userInput > o.ItemsForSale.Count)
+                    Console.WriteLine("To order an item, please enter the number that corresponds with the desired item.");
+                    try
+                    {
+                        userInput = int.Parse(Console.ReadLine());
+                        if (userInput < 0 || userInput > o.ItemsForSale.Count)
+                        {
+                            Console.WriteLine("Sorry, that was not a valid input. Please try again.");
+                            continue;
+                        }
+                        else
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine($"How many {o.ItemsForSale[userInput - 1].ProductName}s would you like to purchase?");
+                                try
+                                {
+                                    desiredQuantity = int.Parse(Console.ReadLine());
+                                    if (desiredQuantity < 1)
+                                    {
+                                        Console.WriteLine("You must enter an integer greater than zero.");
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        o.AddToCart(userInput - 1, desiredQuantity);
+                                        Console.WriteLine($"Okay, that'll be ${Math.Round(desiredQuantity * o.ItemsForSale[userInput-1].Price,2)}");
+                                        Console.WriteLine();
+                                    }
+
+                                    break;
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("Sorry, that was not a valid input. Please try again.");
+                                    continue;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    catch
                     {
                         Console.WriteLine("Sorry, that was not a valid input. Please try again.");
                         continue;
                     }
-                    else
-                    {   while (true)
-                        {   
-                            Console.WriteLine($"How many {o.ItemsForSale[userInput - 1].ProductName}s would you like to purchase?");
-                            try
-                            {
-                                 desiredQuantity = int.Parse(Console.ReadLine());
-                                if (desiredQuantity < 1)
-                                {
-                                    Console.WriteLine("You must enter an integer greater than zero.");
-                                    continue;
-                                }
-                                else
-                                {
-                                    o.AddToCart(userInput -1, desiredQuantity);
-                                }
-
-                                break;
-                            }   
-                            catch
-                            {
-                                Console.WriteLine("Sorry, that was not a valid input. Please try again.");
-                                continue;
-                            }
-                        }
-                        break;
-
-                    }
                 }
-                catch
-                {
-                    Console.WriteLine("Sorry, that was not a valid input. Please try again.");
-                    continue;
-                }
+                runAgain = h.RunAgain();
+                o.PrintRecipt();
             }
-            double total = 0;
-            for (int i = 0; i < o.ItemsPurchased.Count; i++)
-            {
-                Console.WriteLine(o.ItemsPurchased[i].ProductName);
-                total += o.ItemsPurchased[i].Price;
-                
-            }
-            Console.WriteLine($"${ Math.Round(total, 2)}");
             
+
+
+
+           
 
         }
     }
